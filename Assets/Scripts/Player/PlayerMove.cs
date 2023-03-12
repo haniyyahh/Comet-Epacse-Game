ff546b2c9d2c21727b58f54aa8e2a02faa494415
RadZ3000 <86256395+RadZ3000@users.noreply.github.com>
2023-03-12 14:14:29 -0500

    Tobor
    Added Tobor asset

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public float leftRightSpeed = 50;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+
+        // moving left to right within the boundaries of the SandFloor
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Comet-Epacse
+CS 4332 Spring 2023 Project
